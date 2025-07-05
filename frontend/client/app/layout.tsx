b749c2a890038ece2742075479d336af8d2f05a4
deba7 <debayudhbasu@gmail.com>
2025-07-05 12:42:14 +0530

    nextjs adapter added

---
+++ frontend/client/app/layout.tsx
@@ -0,0 +1,42 @@
+import "./globals.css";
+import { WalletProvider } from "@/components/WalletProvider";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import { PropsWithChildren } from "react";
+import { AutoConnectProvider } from "@/components/AutoConnectProvider";
+import { ReactQueryClientProvider } from "@/components/ReactQueryClientProvider";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Medical Records",
+  description:
+    "Aptos Medical Records DApp",
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "flex justify-center min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+       
+          <AutoConnectProvider>
+            <ReactQueryClientProvider>
+              <WalletProvider>
+                {children}
+              </WalletProvider>
+            </ReactQueryClientProvider>
+          </AutoConnectProvider>
+        
+      </body>
+    </html>
+  );
+}
